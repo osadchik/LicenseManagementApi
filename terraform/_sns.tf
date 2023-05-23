8918f5931d338a4f5ca6550f938b1f997dd48b72
Myroslav <osadchiik@gmail.com>
2023-05-23 16:57:19 +0300

    Add SNS rights to publish the message to SQS queue

--- terraform/_sns.tf
+++ terraform/_sns.tf
@@ -3,3 +3,29 @@ module "user-management-lambda-sns" {
 
     sns_name = "${var.prefix}-${var.user_management_lambda_name}-sns"
 }
+
+data "aws_iam_policy_document" "put_to_sqs" {
+  statement {
+    sid    = "PutMessagesToSQS"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["sns.amazonaws.com"]
+    }
+
+    actions   = ["sqs:SendMessage"]
+    resources = [module.user-integration-lambda-sqs.arn]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [module.user-management-lambda-sns.arn]
+    }
+  }
+}
+
+resource "aws_sqs_queue_policy" "users_subscription" {
+  queue_url = module.user-integration-lambda-sqs.url
+  policy = data.aws_iam_policy_document.put_to_sqs.json
+}
--- terraform/modules/sqs/output.tf
+++ terraform/modules/sqs/output.tf
@@ -2,3 +2,8 @@ output "arn" {
     description = "SQS Amazon Resource Number"
     value       = aws_sqs_queue.sqs.arn 
 }
+
+output "url" {
+    description = "The URL for the created Amazon SQS queue"
+    value       = aws_sqs_queue.sqs.id 
+}
