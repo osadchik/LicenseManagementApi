365d61e752525a134a2dfebb4661d595a65d5e37
Myroslav <osadchiik@gmail.com>
2023-05-25 12:50:13 +0300

    Add users dynamo DB table access for UsersIntegrationLambda

--- terraform/_user-integration-lambda-addition.tf
+++ terraform/_user-integration-lambda-addition.tf
@@ -13,7 +13,7 @@ resource "aws_iam_policy" "user_integration_dynamoDB_policy" {
           "dynamodb:PutItem",
           "dynamodb:GetItem"
         ]
-        Resource = [ "${module.dynamodb_state_table.arn}" ]
+        Resources = [ "${module.dynamodb_state_table.arn}", "${module.dynamodb_users_table.arn}" ]
       }
     ]
   })
