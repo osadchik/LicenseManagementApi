c060bf20a80942eac789cfe7c2719765bb4dfd09
Myroslav <osadchiik@gmail.com>
2023-05-27 16:27:02 +0300

    Add lambda names to API Gateway configuration

--- terraform/_api-gateway.tf
+++ terraform/_api-gateway.tf
@@ -2,7 +2,11 @@ module "api_gateway" {
     source      = "./modules/api_gateway"
 
     name         = "${var.prefix}-api-gateway"
-    lambda_names = [ module.user-management-lambda.lambda_name ]
+    lambda_names = [
+        module.user-management-lambda.lambda_name,
+        module.license-management-lambda.lambda_name,
+        module.product-management-lambda.lambda_name
+    ]
     users_uri    = module.user-management-lambda.lambda_invoke_arn
     license_uri  = module.license-management-lambda.lambda_invoke_arn
     products_uri = module.product-management-lambda.lambda_invoke_arn
