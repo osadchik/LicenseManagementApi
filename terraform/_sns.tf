09d9f2c7681a132a97319e46c1b1e115d0e165d6
Myroslav <osadchiik@gmail.com>
2023-06-02 12:31:45 +0300

    Separate sqs policies

--- terraform/_sns.tf
+++ terraform/_sns.tf
@@ -4,7 +4,7 @@ module "user-management-lambda-sns" {
     sns_name = "${var.prefix}-${var.user_management_lambda_name}-sns"
 }
 
-data "aws_iam_policy_document" "put_to_sqs" {
+data "aws_iam_policy_document" "users_integration_put_to_sqs" {
   statement {
     sid    = "PutMessagesToSQS"
     effect = "Allow"
@@ -15,7 +15,7 @@ data "aws_iam_policy_document" "put_to_sqs" {
     }
 
     actions   = ["sqs:SendMessage"]
-    resources = [ module.user-integration-lambda-sqs.arn, module.license-management-lambda-sqs.arn ]
+    resources = [ module.user-integration-lambda-sqs.arn ]
 
     condition {
       test     = "ArnEquals"
@@ -27,10 +27,31 @@ data "aws_iam_policy_document" "put_to_sqs" {
 
 resource "aws_sqs_queue_policy" "users_subscription" {
   queue_url = module.user-integration-lambda-sqs.url
-  policy = data.aws_iam_policy_document.put_to_sqs.json
+  policy = data.aws_iam_policy_document.users_integration_put_to_sqs.json
+}
+
+data "aws_iam_policy_document" "license_management_put_to_sqs" {
+  statement {
+    sid    = "PutMessagesToSQS"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["sns.amazonaws.com"]
+    }
+
+    actions   = ["sqs:SendMessage"]
+    resources = [ module.license-management-lambda-sqs.arn ]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [ module.user-management-lambda-sns.arn]
+    }
+  }
 }
 
 resource "aws_sqs_queue_policy" "license_subscription" {
   queue_url = module.license-management-lambda-sqs.url
-  policy = data.aws_iam_policy_document.put_to_sqs.json
+  policy = data.aws_iam_policy_document.license_management_put_to_sqs.json
 }
