8c9518fae3abc34551d36617dfe52e6ecdaaea54
Myroslav <osadchiik@gmail.com>
2023-06-20 13:52:24 +0300

    Enable swagger annotations

--- Common/Extensions/SwaggerConfigrationExtensions.cs
+++ Common/Extensions/SwaggerConfigrationExtensions.cs
@@ -14,6 +14,7 @@ namespace Common.Extensions
             services.AddSwaggerGen(options =>
             {
                 options.SwaggerDoc("v1", swaggerInfo);
+                options.EnableAnnotations();
 
                 AddSwaggerXmlComments(options);
             });
